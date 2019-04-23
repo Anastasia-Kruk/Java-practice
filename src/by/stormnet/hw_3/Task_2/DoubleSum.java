d353b8426a0e2bcbefac59d847ff755a672bc864
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 21:28:37 +0300

    Update DoubleSum.java

--- src/by/stormnet/hw_3/Task_2/DoubleSum.java
+++ src/by/stormnet/hw_3/Task_2/DoubleSum.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 public class DoubleSum {
     public static void main(String[] args) {
+        // все гуд, имя такета только если еще не исправила все 
         double numD1 = 1.2;
         double numD2 = 1.4;
         BigDecimal bigD1 = BigDecimal.valueOf(numD1);
