7ac22e64efda0dd166670d5f062d4dc4918c900b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 21:35:03 +0300

    Update MathExp.java

--- src/by/stormnet/hw_3/Task_4/MathExp.java
+++ src/by/stormnet/hw_3/Task_4/MathExp.java
@@ -2,11 +2,13 @@ package by.stormnet.hw_3.Task_4;
 
 public class MathExp {
     public static void main(String[] args) {
+        // переменная degree не нужна, ты ее передаешь в Math.toRadians 
         double degree = 180;
         double radians = Math.toRadians(180);
         double cos =Math.cos(radians);
 
 
+        // эти переменные тоже не нужны. Через какое-то время поймешь где нужно выносить, а где можно пренебречь
         int num1 = 256;
         int num2 = 8;
         int num3 = 2;
