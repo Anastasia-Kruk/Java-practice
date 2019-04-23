205f698c7717f94764c78b06abc13e8e68851a90
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 21:30:50 +0300

    Update Frase.java

--- src/by/stormnet/hw_3/Task_3/Frase.java
+++ src/by/stormnet/hw_3/Task_3/Frase.java
@@ -3,9 +3,12 @@ package by.stormnet.hw_3.Task_3;
 public class Frase {
     public static void main(String[] args) {
         String ask = "Whara is my dog+";
+        // так себе имя для переменной + можно сделать в одну строку
+        // ask.replace('a', 'e').replace('+', '?');
         String str = ask.replace('a', 'e');
         String fool = str.replace('+', '?');
 
+        // fool переводится как "дурак" :) гугл переводчик полезная штука
         System.out.println(fool);
     }
 }
