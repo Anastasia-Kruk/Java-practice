da5ba67c2eb5ff19111f4bb9cc2b9e3313223cea
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-01 12:42:26 +0300

    Update RecursArray.java

--- src/by/stormnet/hw_5/task_3/RecursArray.java
+++ src/by/stormnet/hw_5/task_3/RecursArray.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class RecursArray {
     public static void main(String[] args) {
+        // не array, а numbers
         int[] array = new int[10];
         Random random = new Random();
 
@@ -15,17 +16,18 @@ public class RecursArray {
 
         int num = 0;
         recursion(array, num);
-
+// не нужен здесь абзац
     }
 
     static void recursion(int[] arr, int i) {
-
+// не нужен здесь абзац
         if (i < arr.length) {
             System.out.print(arr[i] + ", ");
             i++;
 
             recursion(arr, i);
-
+// не нужен здесь абзац
         }
     }
+    // 9, 3, 6, 9, 8, 2, 8, 7, 9, 3, - это вывод твоей программы, где-то лишняя запятая
 }
