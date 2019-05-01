bb1f57e6366f60b2542a3054463a89064051c7e1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-01 12:47:06 +0300

    Update EnterArgument.java

--- src/by/stormnet/hw_5/task_4/EnterArgument.java
+++ src/by/stormnet/hw_5/task_4/EnterArgument.java
@@ -1,7 +1,15 @@
 package by.stormnet.hw_5.task_4;
 
 public class EnterArgument {
+    // все же метод main() должен быть первым. технически это не ошибка, но он главнее
     static void template(String name, int age, int year, String kingdom) {
+        /* задание выполнялось до лекции про классы, поэтому это просто чтобы ты обратила внимание
+        
+        у тебя переменная temp - это шаблон, которым возможно будут пользоваться другие методы класса 
+        + скорее всего еще и константа, значит, такие штуки надо делать полем (т.е. переменной для всего класса)
+        
+        опять же, я помню когда это ДЗ было, но теперь надо будет делать именно полями класса такие переменные
+        */
         String temp = "Hi, my name is %s. I’m %d years old and I was born in %d. I’m the true ruler of %s!!!";
         System.out.printf(temp, name, age, year, kingdom);
     }
