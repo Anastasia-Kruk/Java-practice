6b893acfbb4da34dac13e39dc26a9cac0b2b2d79
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-01 12:33:50 +0300

    Update Prediction.java

--- src/by/stormnet/hw_5/task_1/Prediction.java
+++ src/by/stormnet/hw_5/task_1/Prediction.java
@@ -5,24 +5,43 @@ import java.util.Scanner;
 
 public class Prediction {
     public static void main(String[] args) {
+        // где используешь переменную, там ее и создаешь
         String ask;
 
-
+// два абзаца думаю многовато, можно разделять на логические разделы, используя 'enter', но одного хватит
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Enter your question: ");
+//            String ask = sc.nextLine();
             ask = sc.nextLine();
 
             if (ask.charAt(ask.length() - 1) != '?') {
                 System.out.println("It's not a question");
             }
 
+            // (ask.charAt(ask.length() - 1) != '?')
+           //эта строка дублируется, значит уже есть проблема в коде. 
+            
+            /*           
+           как насчет сделать так:
+            
+            while(true) {
+            if(ask.charAt(ask.length() - 1) == '?') {
+                 answer();
+                 // return остановит выполнение метода main, а, значит и всей программы
+                 return;
+                }
+            }
+            */
         } while (ask.charAt(ask.length() - 1) != '?');
         answer();
     }
 
+    // этот метод по заданию должен принимать строку и проверять на знак "?" и возвращать true/false, т.е. boolean
     static void answer() {
 
+//         Random random = new Random();
+//         random.nextBoolean() - используй его
         Random random = new Random();
         int ans = random.nextInt((2 - 1) + 1) + 1;
 
