22361793c9c533d5009bac43a7d98256ce3b9a78
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-12 12:06:04 +0300

    Update CarDemo.java

--- src/by/stormnet_hw/hw_7/task_1/CarDemo.java
+++ src/by/stormnet_hw/hw_7/task_1/CarDemo.java
@@ -2,12 +2,14 @@ package by.stormnet_hw.hw_7.task_1;
 
 public class CarDemo {
     public static void main(String[] args) {
+//        diesel
         Engine engine = new Engine(1.5, "disel");
         Transmission transmission = new Transmission("Manual", 5);
         Car reno = new Car("Scenic", "Black", engine, transmission);
 
         System.out.println(reno.toString());
 
+        // удаляй закомментированный код в конечной версии
 //        reno.startMoving();
 
         reno.move();
