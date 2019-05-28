6ee9831c8f401c9579f5eb872d8b7689adf534a4
szakrevskaya <selina.88@mail.ru>
2019-05-28 15:08:32 +0300

    Update Program.java

--- src/tests/test_24_05/task_1/Program.java
+++ src/tests/test_24_05/task_1/Program.java
@@ -2,6 +2,8 @@ package tests.test_24_05.task_1;
 
 public class Program {
     public static void main(String[] args) {
+        //Можно было и по-красивее названия кнопкам придумать :)
+        //Ведь у каждой кнопки свое назначение :)
         Button button1 = new Button(new EventHandler() {
             @Override
             public void execute() {
