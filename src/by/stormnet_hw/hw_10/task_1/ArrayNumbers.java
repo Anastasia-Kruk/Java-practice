1347bfc2d4a0de705fa6ac45a6570735472e7c8a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-10 15:03:27 +0300

    Update ArrayNumbers.java

--- src/by/stormnet_hw/hw_10/task_1/ArrayNumbers.java
+++ src/by/stormnet_hw/hw_10/task_1/ArrayNumbers.java
@@ -1,6 +1,7 @@
 package by.stormnet_hw.hw_10.task_1;
 
 public class ArrayNumbers {
+    // лучше используй не Arr, a T/E - так очевиднее, то это generic
     public static <Arr extends Number> double sum(Arr[] array) {
         double sum = 0;
         for (Arr elem : array) {
