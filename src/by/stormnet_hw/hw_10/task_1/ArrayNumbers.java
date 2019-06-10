b3f7ea0ace5b9a1ca99df17ef790929c532f5202
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-10 15:04:25 +0300

    Update ArrayNumbers.java

--- src/by/stormnet_hw/hw_10/task_1/ArrayNumbers.java
+++ src/by/stormnet_hw/hw_10/task_1/ArrayNumbers.java
@@ -1,5 +1,6 @@
 package by.stormnet_hw.hw_10.task_1;
 
+// название класса все же так себе. ArrayMath, ArrayUtils что-то такое
 public class ArrayNumbers {
     // лучше используй не Arr, a T/E - так очевиднее, то это generic
     public static <Arr extends Number> double sum(Arr[] array) {
