bf64edfc87d1a97968b39058a75e25bafdadf4fc
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-10 15:24:41 +0300

    Update BuildingHouseEntranceComparator.java

--- src/by/stormnet_hw/hw_11/task_3/BuildingHouseEntranceComparator.java
+++ src/by/stormnet_hw/hw_11/task_3/BuildingHouseEntranceComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class BuildingHouseEntranceComparator implements Comparator<Building> {
     @Override
     public int compare(Building a, Building b) {
+        // старайся избегать if-else-if конструкций, лучше разделить на несколько if()
         if (a.getHouseEntrance() > b.getHouseEntrance()) {
             return 1;
         } else if (a.getHouseEntrance() < b.getHouseEntrance()) {
