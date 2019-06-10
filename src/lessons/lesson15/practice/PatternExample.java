defee7131221a18f25880c45eac5302a2448c157
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-10 15:01:40 +0300

    Update PatternExample.java

--- src/lessons/lesson15/practice/PatternExample.java
+++ src/lessons/lesson15/practice/PatternExample.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class PatternExample {
     public static void main(String[] args) {
         String string = "While major game play components are already in place and functioning, players help is needed to add some of the smaller features and content. Of course polishing the overall experience is important at the same time. Game has been extensively tested in closed group, but it is always important to hear fresh suggestions from larger crowd that come from various gaming backgrounds. Plan is to make the game as good as possible but without compromising the core idea.";
+        // list -> wordsToFind
         ArrayList<String> list = new ArrayList<>();
         list.add("alert");
         list.add("add");
