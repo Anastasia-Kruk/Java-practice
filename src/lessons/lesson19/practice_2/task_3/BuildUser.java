702022dd435b345dfd77ba811024fbc768910b59
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-01 18:16:17 +0300

    Update BuildUser.java

--- src/lessons/lesson19/practice_2/task_3/BuildUser.java
+++ src/lessons/lesson19/practice_2/task_3/BuildUser.java
@@ -1,5 +1,6 @@
 package lessons.lesson19.practice_2.task_3;
 
+// BuildUser - это действие, по сути глагол, а имя класса должно быть существительным, т.е UserBuilder
 public interface BuildUser {
     User build(String name);
 }
